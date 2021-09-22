f4fa1056e326fe1d08392cfd6eefc657d70d20f1
Alex Fetea <pvn5nv@virginia.edu>
2021-09-22 15:07:01 +0000

    finished hw 3

--- src/submissions/practice_2.lean
+++ src/submissions/practice_2.lean
@@ -8,7 +8,9 @@ and/or theorems that you use.
 
 example : true := true.intro
 
-example : false := _    -- trick question? why?
+example : false := _  -- trick question? why?
+
+--Their is no intro rule for false.
 
 example : ∀ (P : Prop), P ∨ P ↔ P := 
 begin
@@ -27,6 +29,13 @@ begin
     assume p,
     exact or.intro_left P p,
 end
+/- For problem 1, we start by using the introduction
+rule for if and only if, giving us two different
+propositions. We use the elimination rule to show 
+that P ∨ P implies P. Next we use the introduction
+rule to show that P implies P ∨ P-/
+
+
 
 example : ∀ (P : Prop), P ∧ P ↔ P := 
 begin
@@ -41,9 +50,14 @@ begin
     apply and.intro _ _,
     apply prop,
     apply prop,
- 
-
 end
+/- For problem 2, we start by using the introduction
+rule for if and only if, giving us two different
+propositions. We use the elimination rule to show 
+that P ∧ P implies P. Next we use the introduction
+rule to show that P implies P ∧ P-/
+
+
 
 example : ∀ (P Q : Prop), P ∨ Q ↔ Q ∨ P := 
 begin
@@ -67,8 +81,17 @@ exact p,
 assume q,
 apply or.intro_left,
 exact q,
-
 end
+/- For problem 3, we start by using the introduction
+rule for if and only if, giving us two different
+propositions. We use the elimination rule giving us 
+the propositions P → Q ∨ P and Q → Q ∨ P. We use the
+intro rule for or to prove the first proposition, and 
+then use it again to prove the second proposition. To
+solve backwards, we first apply the elimination rule 
+for or, giving us Q → P ∨ Q and P → P ∨ Q, and then we
+apply the intro rule for or to both propositions-/
+
 
 
 example : ∀ (P Q : Prop), P ∧ Q ↔ Q ∧ P := 
@@ -91,8 +114,15 @@ begin
   apply and.intro _ _,
   exact p,
   exact q,
-
 end
+/- For problem 4, we start by using the introduction
+rule for if and only if. We then apply the elimination
+rule for and, giving us P → Q → Q ∧ P. We then apply
+the intro rule for and. Solving backwords, we apply the
+elimination rule for and, giving us Q → P → P ∧ Q. We 
+then use the introduction rule for and.-/
+
+
 
 example : ∀ (P Q R : Prop), P ∧ (Q ∨ R) ↔ (P ∧ Q) ∨ (P ∧ R) := 
 begin
@@ -135,6 +165,24 @@ exact p,
 apply or.intro_right,
 exact r,
 end
+/- For problem 5, we start by using the introduction
+rule for if and only if. We then apply the elimination
+rule for and, giving us P → Q ∨ R → P ∧ Q ∨ P ∧ R. We 
+then apply the elimination rule for or, giving us 
+Q → P ∧ Q ∨ P ∧ R and R → P ∧ Q ∨ P ∧ R. intro rule for
+and. We then apply the introrule for or, followed by the
+intro rule for and, solving the firstproposition. We
+apply the intro rule for or, followed by the intro rule
+for and again, solving the seond propositions. Solving 
+backwords, we apply the elimination rule for or, then the
+elimination rule for and, giving us the proposition
+P → Q → P ∧ (Q ∨ R). We then apply the intro rule for and
+and the intro rule for or, solving the proposition. Next,
+we apply the elimination rule for and, giving us the
+proposition P → R → P ∧ (Q ∨ R). We finally apply the intro
+rule for and, then the intro rule for or. -/
+
+
 
 example : ∀ (P Q R : Prop), P ∨ (Q ∧ R) ↔ (P ∨ Q) ∧ (P ∨ R) := 
 begin
@@ -160,26 +208,41 @@ begin
   exact r,
 
   assume prop,
-  apply and.elim prop,
-  assume prop2,
-  assume prop3,
-
-  have r: R  := or.elim_right prop2,
-  apply or.elim prop2,
+  have pq :(P ∨ Q) := and.elim_left prop,
+  have pr :(P ∨ R) := and.elim_right prop,
+  apply or.elim pq,
   assume p,
   apply or.intro_left,
   exact p,
-
   assume q,
+  apply or.elim pr,
+  assume p,
+  apply or.intro_left,
+  exact p,
+  assume r,
   apply or.intro_right,
   apply and.intro,
   exact q,
-  
-  apply or.elim,
-  assume p,
-
-
+  exact r,
 end
+/- For problem 6, we start by using the introduction
+rule for if and only if. We then apply the elimination
+rule for and, giving us P → (P ∨ Q) ∧ (P ∨ R). We then
+apply the intro rule for and to serperate the left and 
+right statements. Each statement is then proven with the
+intro rule for or. We use the eliminiation rule for or
+to get Q → R → (P ∨ Q) ∧ (P ∨ R). We then apply the intro
+rule for and, to seperate the statements. We then apply
+the intro rule for or to each of these statements.
+Solving backwords, we let pq represent the left half of
+proposition using the elimination rule, and pr to 
+represent the right have. using the elimination rule for
+or on pq we are left with the statement P → P ∨ Q ∧ R. We
+can then apply the intro rule for or to seperate the two
+statements, followed by the intro rule for and to solve
+the statement on the right.-/
+
+
 
 example : ∀ (P Q : Prop), P ∧ (P ∨ Q) ↔ P := 
 begin
@@ -196,6 +259,15 @@ begin
   apply or.intro_left,
   exact p,
 end
+/- For problem 7, we start by using the introduction
+rule for if and only if. We then apply the elimination
+rule for and, giving us P ∧ (P ∨ Q) → P. We then
+apply the elimination rule for and to serperate the left and 
+right statements. We then use the intro rule for and to 
+the left half of the proposition, and then apply the intro
+rule for or to solve the right half.-/
+
+
 
 example : ∀ (P Q : Prop), P ∨ (P ∧ Q) ↔ P := 
 begin
@@ -214,6 +286,15 @@ begin
   apply or.intro_left,
   exact p,
 end
+/- For problem 8, we start by using the introduction
+rule for if and only if. We then apply the elimination
+rule for or, giving us P ∧ (P ∨ Q) → P. We then
+apply the elimination rule for and to serperate the left and 
+right statements. We then apply the elimination rule for and
+to seperate teh left and right statements. We finally apply 
+the intro rule for or to solve the proposition.-/
+
+
 
 example : ∀ (P : Prop), P ∨ true ↔ true := 
 begin
@@ -225,6 +306,13 @@ begin
   apply or.intro_right,
   exact t,
 end
+/- For problem 8, we start by using the introduction
+rule for if and only if. We use the intro rule for 
+true to prov the first proposition. We use the intro
+rule for or to prove the statement or the right hand
+side true.-/
+
+
 
 example : ∀ (P : Prop), P ∨ false ↔ P := 
 begin
@@ -238,8 +326,12 @@ begin
   assume p,
   apply or.intro_left,
   exact p, 
-
 end
+/- For problem 9, we start by using the introduction
+rule for if and only if. We then apply the elimination
+rule for or. We then apply the elimination rule for
+false, proving one of the statements. We finally apply
+the intro rule for or, proving the left hand side.-/
 
 example : ∀ (P : Prop), P ∧ true ↔ P := 
 begin
@@ -256,6 +348,13 @@ begin
   exact p,
   exact true.intro,
 end
+/- For problem 10, we start by using the introduction
+rule for if and only if. We then apply the elimination
+rule for and to solve the first proposition. We then use
+the introduction rule for and followed by the introduction
+rule for true to solve the second proposition.-/
+
+
 
 example : ∀ (P : Prop), P ∧ false ↔ false := 
 begin
@@ -268,5 +367,10 @@ begin
   exact f,
   apply false.elim,
 end
+/- For problem 11, we start by using the introduction
+rule for if and only if. We then apply the elimination
+rule for and to solve the first proposition. We then use
+the elimination rule for flase to solve the second 
+proposition.-/
 
 
